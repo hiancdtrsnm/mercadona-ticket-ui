72d411e47b291205974475836bec2ec1251c1710
Hian Cañizares <hiancdtrsnm@gmail.com>
2024-07-29 08:31:30 +0200

    Remove Unused line

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-import { loadEnv } from "vite";
 export const SITE_TITLE = import.meta.env.APP_TITLE || 'Mercadona Ticket'
 export const SITE_DESCRIPTION = 'Una forma rapida y sencilla de convertir tus tickets de Mercadona en un CSV'
 export const API_ENDPOINT = import.meta.env.PUBLIC_API_ENDPOINT || 'https://api.mercadona-ticket.nubecita.dev/upload_ticket'
